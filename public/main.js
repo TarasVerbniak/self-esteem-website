97484d448ca46e20e90135fc5c0c1806aa0ab068
TarasVerbniak <taras.verbniak@gmail.com>
2017-12-12 00:14:36 +0200

    toggle active nav class

--- public/main.js
+++ public/main.js
@@ -1,6 +1,29 @@
 $( document ).ready(function(){
+    /* ACTIVATE COLLAPSIBLE NAVBAR */
     $(".button-collapse").sideNav({
-        menuWidth: '80%', // Default is 240
+        menuWidth: '80%',
         closeOnClick: true
     });
+
+    /* TOGGLE ACTIVE NAVBAR CLASS */
+    const currentUrl = window.location.pathname;
+    let currentNavLi;
+    switch (currentUrl) {
+        case '/self':
+            currentNavLi = $('#self');
+            break;
+        case '/love':
+            currentNavLi = $('#love');
+            break;
+        case '/career':
+            currentNavLi = $('#career');
+            break;
+        case '/family':
+            currentNavLi = $('#family')
+            break;    
+        default:
+            currentNavLi = $('#home');
+            break;
+    }
+    currentNavLi.addClass('active');
 });
